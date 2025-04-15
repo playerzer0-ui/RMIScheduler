187a9fdb2c742e1c58709b6ae804ab397643b85c
player_zero <playerzero745@gmail.com>
2025-04-15 16:12:06 +0100

    manual lambda is BEST

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,9 @@ public class Main {
 
         //start a scheduler
         ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-        //get a method there
-        executor.scheduleAtFixedRate(Main::yap, 0, 1000, TimeUnit.MILLISECONDS);
-        //all methods start at a certain period (it will display the scheduler at the most top first)
-        executor.scheduleAtFixedRate(Main::yap2, 0, 1000, TimeUnit.MILLISECONDS);
-        //you can also manually create a function inside the method
+        //manually use this function here
         executor.scheduleAtFixedRate(()->{
             System.out.println("guess the manual works");
         }, 0, 2000, TimeUnit.MILLISECONDS);
-    }
-
-    public static void yap(){
-        System.out.println("I'mma yap");
-    }
-
-    public static void yap2(){
-        System.out.println("I'mma yap2");
     }
 }
